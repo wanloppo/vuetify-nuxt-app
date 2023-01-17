b53dad64cddc591ed55fbdf92a25126a1a76505e
wanlop poompanich <wanloppo@gmail.com>
2023-01-17 08:18:26 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet color="grey lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
+
+        <div>wanloppo@gmail.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item v-for="[icon, text] in links" :key="icon" link>
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row class="bg-grey">
+          <template v-for="n in 4" :key="n">
+            <v-col class="mt-2" cols="12">
+              <strong>Category {{ n }}</strong>
+            </v-col>
+
+            <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
+              <v-sheet height="150"></v-sheet>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<!-- <script>
+export default {
+  data: () => ({ drawer: null }),
+};
+</script> -->
+
+
+<script>
+export default {
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+    drawer: null,
+    links: [
+      ["mdi-inbox-arrow-down", "Inbox"],
+      ["mdi-send", "Send"],
+      ["mdi-delete", "Trash"],
+      ["mdi-alert-octagon", "Spam"],
+    ],
+  }),
+};
+</script>
---
+++ v-row.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container class="bg-surface-variant">
+    <v-row class="bg-green" v-for="j in 6" :key="j" no-gutters >
+      <v-col
+        v-for="n in 3"
+        :key="`${j}${n}`"
+        cols="12"
+        sm="4"
+      >
+        <v-sheet class="ma-2 pa-2">
+          {{j}}.{{n}} of three columns
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
